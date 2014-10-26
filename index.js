0bcee0d7107377d1bf63a648bb5439f4198dc256
ruchiraw <ruchira.wageesha@gmail.com>
2014-10-26 15:32:19 +0530

    updating

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 var dust = require('dust')();
 var serand = require('serand');
-var io = require('socket.io');
 
 var consol;
-
-var HUB = 'wss://hub.serandives.com:4000/app';
-
-/*var hub = io.connect(HUB, {
-    transports: ['websocket']
-});
-
-hub.once('connect', function () {
-    hub.on('done', function (options) {
-        console.log(options);
-        consol.append(options.data);
-    });
-});*/
 
 dust.loadSource(dust.compile(require('./template'), 'hub-server-details'));
 dust.loadSource(dust.compile(require('./update'), 'hub-server-details-update'));
@@ -112,7 +98,7 @@ module.exports = function (sandbox, fn, options) {
 };
 
 /*serand.on('hub', 'drones start', function (data) {
-    console.log('received event');
-    console.log(data);
-    hub.emit('drones start', data);
-});*/
+ console.log('received event');
+ console.log(data);
+ hub.emit('drones start', data);
+ });*/
